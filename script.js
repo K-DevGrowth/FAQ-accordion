1fd5615f6cc757a14b9b228ef62cc5da4f000690
Khoi <khoidang01234@gmail.com>
2025-05-20 21:39:25 +0700

    Add html, css and js files

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get all elements with class
+const elements = () => {
+    const acc = document.querySelectorAll('.faqs-btn');
+    const panels = document.querySelectorAll('.panel');
+    const minusIcons = document.querySelectorAll('.minus-icon');
+    const plusIcons = document.querySelectorAll('.plus-icon');
+    const addActive = [panels, minusIcons, plusIcons]
+
+    return { acc, addActive };
+}
+
+// Toggle between hiding and showing the 'active' accordion panel and 'active' icon
+elements().acc.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        elements().addActive.forEach(e => e[index].classList.toggle('active'));
+    })
+})
+
+// Get the element with id="defaultOpen" and click on it
+document.querySelector('#defaultOpen').click();
